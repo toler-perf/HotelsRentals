20a1b806aca8bff65a77506d375e90ac7f433ddd
toler-perf <etolstykh.toler@gmail.com>
2026-02-16 18:22:44 +0300

    Чистка main

--- app/main.py
+++ app/main.py
@@ -9,24 +9,3 @@ from app.bookings.router import router as router_bookings
 app = FastAPI()
 app.include_router(router_bookings)
 
-
-class HotelsSearchArgs(BaseModel):
-    def __init__(
-        self, 
-        location: str, 
-        date_from: date, 
-        date_to: date, 
-        guests: int, 
-        stars: Optional[int] = None
-    ):
-        self.location = location
-        self.date_from = date_from
-        self.date_to = date_to
-        self.guests = guests
-        self.stars = stars
-
-@app.get("/hotels")
-def get_hotels(
-    search_args: HotelsSearchArgs = Depends()
-):
-    return search_args
