f5caa3571d730d2a2474cfd769f9199a5c282bff
toler-perf <etolstykh.toler@gmail.com>
2026-02-16 13:56:57 +0300

    modified:   app/main.py

--- app/main.py
+++ app/main.py
@@ -1,12 +1,40 @@
-from fastapi import FastAPI
+from datetime import date
+from typing import Optional
+from fastapi import FastAPI, Query, Depends
+from pydantic import BaseModel, Field
 
 
 app = FastAPI()
 
 
-@app.get("/hotels/{hotel_id}")
+class HotelsSearchArgs(BaseModel):
+    def __init__(
+        self, 
+        location: str, 
+        date_from: date, 
+        date_to: date, 
+        guests: int, 
+        stars: Optional[int] = None
+    ):
+        self.location = location
+        self.date_from = date_from
+        self.date_to = date_to
+        self.guests = guests
+        self.stars = stars
+
+@app.get("/hotels")
 def get_hotels(
-    hotel_id: int
+    search_args: HotelsSearchArgs = Depends()
 ):
-    return "Hotel"
+    return search_args
+
+
+class SBokking(BaseModel):
+    room_id: int
+    date_from: date
+    date_to: date
+
+@app.post("/bookings")
+def add_booking(booking: SBokking):
+    pass
 
